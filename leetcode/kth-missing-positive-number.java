492868f77d269436c9152856b7490d4a661e4fc8
chetangfg-0311 <chetangfg@gmail.com>
2026-08-22 19:11:43 +0530

    Update LeetCode solution: Kth Missing Positive Number

--- leetcode/kth-missing-positive-number.java
+++ leetcode/kth-missing-positive-number.java
@@ -1,28 +1,10 @@
-1class Solution {
-2    public int findKthPositive(int[] arr, int k) {
-3        int low = 0;
-4        int high = arr.length - 1;
-5        int mid;
-6
-7        if(k < arr[0]){
-8            return k;
-9        }
-10
-11        if(k == 1 && arr[0] != 1){
-12            return 1;
-13        }
-14
-15        while(low <= high){
-16            mid = ((high - low) / 2) + low;
-17            int missingNo = arr[mid] - mid - 1;
-18
-19            if(missingNo >= k){
-20                high = mid - 1;
-21            }else{
-22                low = mid + 1;
-23            }
-24        }
-25        int ans = arr[high] + k - (arr[high] - high - 1);
-26        return ans;
-27    }
-28}
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int n=arr.length;
+        for(int num:arr){
+            if(num<=k)  k++;
+            else  break;
+        }
+        return k;
+    }
+}
