62303d2f4faae0f8d19b89137617cb19618a2b17
chetangfg-0311 <chetangfg@gmail.com>
2026-08-23 10:45:36 +0530

    LeetCode: Find the Smallest Divisor Given a Threshold

--- leetcode/find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ leetcode/find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,32 +1,21 @@
-1class Solution {
-2    public int smallestDivisor(int[] nums, int threshold) {
-3        int low = 1;
-4        int high = 1;
-5        int mid;
-6
-7        for(int num : nums){
-8            high = Math.max(num, high);
-9        }
-10
-11        while (low <= high){
-12            mid = ((high - low) / 2) + low;
-13            int sumDivisor = 0;
-14
-15            for(int i = 0; i < nums.length; i++){
-16                if(nums[i] % mid == 0){
-17                    sumDivisor += nums[i] / mid;
-18                }else{
-19                    sumDivisor += (nums[i] / mid) + 1;
-20                }
-21            }
-22
-23            if(sumDivisor > threshold){
-24                low = mid + 1;
-25            }else{
-26                high = mid - 1;
-27            }
-28        }
-29
-30        return low;
-31    }
-32}
+mid = ((high - low) / 2) + low;
+            int sumDivisor = 0;
+
+            for(int i = 0; i < nums.length; i++){
+                if(nums[i] % mid == 0){
+                    sumDivisor += nums[i] / mid;
+                }else{
+                    sumDivisor += (nums[i] / mid) + 1;
+                }
+            }
+
+            if(sumDivisor > threshold){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
+}
