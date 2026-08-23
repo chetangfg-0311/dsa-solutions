983496e29375cb03d422c7d4885364bdaad4a4b3
chetangfg-0311 <chetangfg@gmail.com>
2026-08-23 11:00:57 +0530

    LeetCode: Two Sum

--- leetcode/two-sum/two-sum.java
+++ leetcode/two-sum/two-sum.java
@@ -1,5 +1,16 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        
-    }
-}
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        int [] res = new int[2];
+4        HashMap<Integer, Integer> map = new HashMap();
+5
+6        for(int i = 0; i < nums.length; i++){
+7            if(map.containsKey(target - nums[i])){
+8                res[0] = map.get(target - nums[i]);
+9                res[1] = i;
+10            }
+11            map.put(nums[i], i);
+12        }
+13
+14        return res;
+15    }
+16}
