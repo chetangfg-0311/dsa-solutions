2e77eaa41470d268f03ac2d3a29cb8263b2a4fca
chetangfg-0311 <chetangfg@gmail.com>
2026-08-25 16:53:37 +0530

    LeetCode: Split Array Largest Sum

--- leetcode/split-array-largest-sum/split-array-largest-sum.java
+++ leetcode/split-array-largest-sum/split-array-largest-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        int low = 0;
-        l high = 0;
+        long low = 0;
+        long high = 0;
 
         for (int num : nums){
 
