d366227e27f9742312b6d0c91b8c7dacc8b0c8ff
chetangfg-0311 <chetangfg@gmail.com>
2026-08-25 16:09:51 +0530

    LeetCode: Maximum Candies Allocated to K Children

--- leetcode/maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
+++ leetcode/maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -1,12 +1,12 @@
 1class Solution {
 2    public int maximumCandies(int[] candies, long k) {
-3        int low = Integer.MAX_VALUE;
+3        int low = 0;
 4        int high = 0;
 5        int mid;
 6        int sum = 0;
 7
 8        for(int candy : candies){
-9            low = Math.min(candy, low);
+9            
 10            high = Math.max(candy, high);
 11            sum += candy;
 12        }
