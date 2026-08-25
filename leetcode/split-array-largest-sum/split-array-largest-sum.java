a9b18cc7c1e1d8165a4ade78f3e151cd96e9bd5f
chetangfg-0311 <chetangfg@gmail.com>
2026-08-25 17:18:18 +0530

    LeetCode: Split Array Largest Sum

--- leetcode/split-array-largest-sum/split-array-largest-sum.java
+++ leetcode/split-array-largest-sum/split-array-largest-sum.java
@@ -1,32 +1,32 @@
-1class Solution {
-2    public int splitArray(int[] nums, int k) {
-3        long low = nums[0];
-4        long high = 0;
-5        long mid = 0;
-6
-7        for (int num : nums){
-8            high += num ;
-9        }
-10
-11        while (low < high){
-12            mid = ((high - low) / 2) + low;
-13            long sum = 0;
-14            int subArrays = 1;
-15            for(int num : nums){
-16                sum += num;
-17
-18                if(sum > mid){
-19                    sum = num;
-20                    subArrays++;
-21                }
-22            }
-23
-24            if(subArrays > k){
-25                low = mid + 1;
-26            }else{
-27                high = mid - 1;
-28            }
-29        }
-30        return (int)low;
-31    }
-32}
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        long low = 0;
+        long high = 0;
+        long mid;
+
+        for (int num : nums){
+            low = Math.max(num, low);
+            high += num ;
+        }
+
+        while (low <= high){
+            mid = ((high - low) / 2) + low;
+            System.out.println(mid);
+            long sum = 0;
+            int subArrays = 1;
+            for(int num : nums){
+                sum += num;
+
+                if(sum > mid){
+                    sum = num;
+                    subArrays++;
+                }
+            }
+
+            if(subArrays > k){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return (int)low;
