b3d5246918487fcae2370d3d1feb717343783998
chetangfg-0311 <chetangfg@gmail.com>
2026-08-27 16:50:16 +0530

    LeetCode: Remove Outermost Parentheses

--- leetcode/remove-outermost-parentheses/remove-outermost-parentheses.java
+++ leetcode/remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,24 +1,38 @@
-n {
-    public String removeOuterParentheses(String s) {
-        int count = 0;
-        int outerFirst = 0;
-        int i = 0;
-        while(i < s.length()){
-            if(s.charAt(i) == '('){
-                count++;
-            }else{
-                count--;
-            }
-
-            if(count == 0){
-                System.out.println("count 0 index " + i);
-                s  = s.substring(outerFirst + 1, i) + s.substring(i + 1);
-                outerFirst = i + 1;
-                i = i - 2;
-            }
-            i++;
-        }
-
-        return s;
-    }
-}
+1class Solution {
+2    public String removeOuterParentheses(String s) {
+3       Stack<Integer> st = new Stack<>();
+4        String str="";
+5        int idx=0;
+6        int start =1;
+7        
+8        while(idx<s.length()){
+9            while(s.charAt(idx)=='('){
+10                st.push(idx);
+11                idx++;
+12            }
+13            while(!st.empty() && s.charAt(idx)==')'){
+14                st.pop();
+15                idx++;
+16            }
+17            if(st.empty()){
+18                str += s.substring(start,idx-1);
+19                start = idx+1;
+20            }
+21        }
+22//         for(int i=0;i<s.length();i++){
+23//             char c = s.charAt(i);
+24//             if(c =='('){
+25//                 st.push(i);
+26//             }
+27//             else if( c==')'){
+28//               idx= st.pop();
+29                
+30//                 if(st.empty()){
+31//                     str += s.substring(idx+1,i);
+32//                 }
+33//             }
+34//         }
+35        
+36        return str ;
+37    }
+38}
