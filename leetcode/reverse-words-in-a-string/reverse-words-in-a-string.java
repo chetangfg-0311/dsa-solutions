3caca90bdccaea168b30364ca7790bf09d86ae08
chetangfg-0311 <chetangfg@gmail.com>
2026-08-27 17:00:45 +0530

    LeetCode: Reverse Words in a String

--- leetcode/reverse-words-in-a-string/reverse-words-in-a-string.java
+++ leetcode/reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,5 +1,7 @@
 class Solution {
     public String reverseWords(String s) {
-        for(int i = s.length() - 1; i >)
+        for(int i = s.length() - 1; i >= 0; i--){
+            
+        }
     }
 }
