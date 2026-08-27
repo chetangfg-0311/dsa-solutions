7bf2b7e0a9cc67cd9f111ee0f8e256ee2e34482c
chetangfg-0311 <chetangfg@gmail.com>
2026-08-27 17:03:41 +0530

    LeetCode: Reverse Words in a String

--- leetcode/reverse-words-in-a-string/reverse-words-in-a-string.java
+++ leetcode/reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -4,7 +4,7 @@ class Solution {
         int endIdx = s.length - 1;
 
         for(int i = s.length() - 1; i >= 0; i--){
-            if(s.charat(i) == ' ' && i ){
+            if(s.charat(i) == ' ' && i != endIdx){
                 result.append(s.subString)
             }
         }
