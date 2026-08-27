1d45a29d90295dc7217066cea2a33785df3494ca
chetangfg-0311 <chetangfg@gmail.com>
2026-08-27 16:33:15 +0530

    LeetCode: Remove Outermost Parentheses

--- leetcode/remove-outermost-parentheses/remove-outermost-parentheses.java
+++ leetcode/remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,5 +1,20 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        return s;
+        int count = 0;
+        int outerFirst = 0;
+        String ans = "";
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }else{
+                count--;
+            }
+
+            if(count == 0){
+                ans  += s.substring(outerFirst + 1, i) + s.substring(i + 1);
+            }
+        }
+
+        return ans;
     }
 }
