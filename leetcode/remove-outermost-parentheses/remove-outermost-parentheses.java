e4235f8210956085b7580a5b9489947810fefb16
chetangfg-0311 <chetangfg@gmail.com>
2026-08-27 16:36:16 +0530

    LeetCode: Remove Outermost Parentheses

--- leetcode/remove-outermost-parentheses/remove-outermost-parentheses.java
+++ leetcode/remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -11,7 +11,7 @@ class Solution {
             }
 
             if(count == 0){
-                System.out.println("count 0 index ")
+                System.out.println("count 0 index " + i);
                 ans  += s.substring(outerFirst + 1, i) + s.substring(i + 1);
             }
         }
