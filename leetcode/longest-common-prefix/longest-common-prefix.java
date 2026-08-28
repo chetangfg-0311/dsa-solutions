8e55d790a734ccb6350621aeb3474586fa10febf
chetangfg-0311 <chetangfg@gmail.com>
2026-08-28 16:41:52 +0530

    LeetCode: Longest Common Prefix

--- leetcode/longest-common-prefix/longest-common-prefix.java
+++ leetcode/longest-common-prefix/longest-common-prefix.java
@@ -9,7 +9,7 @@ class Solution {
         Arrays.sort(strs);
 
         for(int i = 0; i < Math.min(strs[0].length(), strs[strs.length - 1].length()); i++){
-            if(strs[0].charAt[i] != strs[strs.len ].charAt[i]){
+            if(strs[0].charAt[i] != strs[strs.length - 1].charAt[i]){
                 return ans.toString();
             }
             ans.append(strs[0])
