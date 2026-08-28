3eb14b5f094afc3b0332f3657e4b105cd0fb9c56
chetangfg-0311 <chetangfg@gmail.com>
2026-08-28 20:40:58 +0530

    LeetCode: Split Array Largest Sum

--- leetcode/split-array-largest-sum/split-array-largest-sum.java
+++ leetcode/split-array-largest-sum/split-array-largest-sum.java
@@ -1,48 +1,32 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        if(k>nums.length){
-            return -1;
+        long low = 0;
+        long high = 0;
+        long mid;
+
+        for (int num : nums){
+            low = Math.max(num, low);
+            high += num ;
         }
-        int sum=0;
-        int max=0;
-        for(int x:nums){
-            if(x>max){
-                max=x;
-            }
-            sum+=x;
-        }
-        if(k==nums.length){
-            return max;
-        }
-        int l=max,h=sum;
-        int res=0;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(ispossible(nums,k,mid)){
-                res=mid;
-                h=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-          return res;
-        }
-        public boolean ispossible(int[] nums,int k,int mid){
-            int stu=0;
-            int student=1;
-            for(int i=0;i<nums.length;i++){
-                if(stu+nums[i]<=mid){
-                    stu=stu+nums[i];
-                }
-                else{
-                    student++;
-                     stu=nums[i];
-                    if(student>k){
-                        return false;
-                    }
+
+        while (low <= high){
+            mid = ((high - low) / 2) + low;
+            System.out.println(mid);
+            long sum = 0;
+            int subArrays = 1;
+            for(int num : nums){
+                sum += num;
+
+                if(sum > mid){
+                    sum = num;
+                    subArrays++;
                 }
             }
-            return true;
+
+            if(subArrays > k){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
         }
-}
+        return (int)low;
