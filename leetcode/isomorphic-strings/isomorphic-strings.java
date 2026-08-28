8645ca586c6fcb8605d1016cb7dd3b2ad5ba1de2
chetangfg-0311 <chetangfg@gmail.com>
2026-08-28 17:02:40 +0530

    LeetCode: Isomorphic Strings

--- leetcode/isomorphic-strings/isomorphic-strings.java
+++ leetcode/isomorphic-strings/isomorphic-strings.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length() != t.length())
+        if(s.length() != t.length()){
+            return false;
+        }
         HashMap<Character, Character> map = new HashMap<>();
 
         for()
