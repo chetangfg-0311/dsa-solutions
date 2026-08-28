4c4014d3e676bce7b2e0bbe69b05f4dd02ec40d1
chetangfg-0311 <chetangfg@gmail.com>
2026-08-28 20:47:38 +0530

    LeetCode: Rotate String

--- leetcode/rotate-string/rotate-string.java
+++ leetcode/rotate-string/rotate-string.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
 
-        if(s.length() != go) 
+        if(s.length() != goal.length()){
+            
+        } 
         s = s+s;
 
         if(s.contains(goal)){
