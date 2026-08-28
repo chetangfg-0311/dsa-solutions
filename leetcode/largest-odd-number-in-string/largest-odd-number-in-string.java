cc454086f8de24b2bdb2d87065f574d29b9743c0
chetangfg-0311 <chetangfg@gmail.com>
2026-08-28 16:11:40 +0530

    LeetCode: Largest Odd Number in String

--- leetcode/largest-odd-number-in-string/largest-odd-number-in-string.java
+++ leetcode/largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -2,7 +2,7 @@ class Solution {
     public String largestOddNumber(String num) {
        String result = "";
 
-        for(char c : num.toCharArray()){
+        for(){
             if(c % 2 != 0){
                 if(result == "" || c > result.charAt(0)){
                     result = Character.toString(c);
