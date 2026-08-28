624be2c6891a22fbcaed9ec35bb2b18983be0610
chetangfg-0311 <chetangfg@gmail.com>
2026-08-28 17:06:00 +0530

    LeetCode: Isomorphic Strings

--- leetcode/isomorphic-strings/isomorphic-strings.java
+++ leetcode/isomorphic-strings/isomorphic-strings.java
@@ -10,7 +10,7 @@ class Solution {
                 return false;
             }
 
-            map.put(s.charAt(i), t.char)
+            map.put(s.charAt(i), t.charAt(i))
         }
         return true;
     }
