700e191785b4871cf1528cc01769127869065a8a
chetangfg-0311 <chetangfg@gmail.com>
2026-08-29 16:38:05 +0530

    LeetCode: Roman to Integer

--- leetcode/roman-to-integer/roman-to-integer.java
+++ leetcode/roman-to-integer/roman-to-integer.java
@@ -1,26 +1,36 @@
-1class Solution {
-2    public int romanToInt(String s) {
-3        HashMap<Character, Integer> map = new HashMap<>();
-4
-5        map.put('I', 1);
-6        map.put('V', 5);
-7        map.put('X', 10);
-8        map.put('L', 50);
-9        map.put('C', 100);
-10        map.put('D', 500);
-11        map.put('M', 1000);
-12
-13        int sum = map.get(s.charAt(s.length() - 1));
-14
-15        for(int i = s.length() - 2; i >= 0; i--){
-16            if(map.get(s.charAt(i)) >= map.get(s.charAt(i + 1))){
-17                sum += map.get(s.charAt(i));
-18            }
-19            else{
-20                sum -= map.get(s.charAt(i));
-21            }
-22        }
-23
-24        return sum;
-25    }
-26}
+class Solution {
+    public int romanToInt(String s) {
+       int ans = 0, num = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            switch (s.charAt(i)) {
+                case 'I':
+                    num = 1;
+                    break;
+                case 'V':
+                    num = 5;
+                    break;
+                case 'X':
+                    num = 10;
+                    break;
+                case 'L':
+                    num = 50;
+                    break;
+                case 'C':
+                    num = 100;
+                    break;
+                case 'D':
+                    num = 500;
+                    break;
+                case 'M':
+                    num = 1000;
+                    break;
+            }
+            if (4 * num < ans)
+                ans -= num;
+            else
+                ans += num;
+        }
+        return ans;
+ 
+    }
+}
