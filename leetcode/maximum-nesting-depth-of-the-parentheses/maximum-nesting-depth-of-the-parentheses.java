c9662ba1d8b5ca18d98cc87d211193a8ebe43c96
chetangfg-0311 <chetangfg@gmail.com>
2026-08-29 16:12:56 +0530

    LeetCode: Maximum Nesting Depth of the Parentheses

--- leetcode/maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
+++ leetcode/maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -1,18 +1,18 @@
-class Solution {
-    public int maxDepth(String s) {
-        int count = 0;
-        int ans = 0;
-
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '('){
-                count++;
-                ans = Math.max(ans, count);
-            }
-            else if(s.charAt(i) == ')'){
-                count--;
-            }
-        }
-
-        return ans;
-    }
-}
+1class Solution {
+2    public int maxDepth(String s) {
+3        int count = 0;
+4        int ans = 0;
+5
+6        for(int i = 0; i < s.length(); i++){
+7            if(s.charAt(i) == '('){
+8                count++;
+9                ans = Math.max(ans, count);
+10            }
+11            else if(s.charAt(i) == ')'){
+12                count--;
+13            }
+14        }
+15
+16        return ans;
+17    }
+18}
