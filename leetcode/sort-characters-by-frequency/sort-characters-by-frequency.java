0551d24b29f10054a2d80e6f9fe610a876665ea1
chetangfg-0311 <chetangfg@gmail.com>
2026-08-29 15:42:55 +0530

    LeetCode: Sort Characters By Frequency

--- leetcode/sort-characters-by-frequency/sort-characters-by-frequency.java
+++ leetcode/sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,5 +1,21 @@
-class Solution {
-    public String frequencySort(String s) {
-        
+1);
+        }
+
+        Pair[] pairs = new Pair[map.size()];
+        int idx = 0;
+        for(char c : map.keySet()){
+            pairs[idx] = new Pair(map.get(c), c);
+            idx++;
+        }
+
+        Arrays.sort(pairs, comp);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < pairs.length; i++){
+            int frequency = pairs[i].freq;
+            while (frequency != 0){
+                sb.append(pairs[i].alpha);
+                frequency--;
+            }
+        }
     }
 }
