3e6518915158087be1ba8212ac80f802232abcdc
chetangfg-0311 <chetangfg@gmail.com>
2026-08-29 16:54:16 +0530

    LeetCode: String to Integer (atoi)

--- leetcode/string-to-integer-atoi/string-to-integer-atoi.java
+++ leetcode/string-to-integer-atoi/string-to-integer-atoi.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '-' || (s.charAt(i) >='0' && s.charAt(i) <= '9')){
                 sb.append(s.charAt(i));
-                if
             }
         }
 
