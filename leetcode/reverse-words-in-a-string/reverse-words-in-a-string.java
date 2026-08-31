041153ef44bec8155ff1ccac6c1f2aedebde4e32
chetangfg-0311 <chetangfg@gmail.com>
2026-08-31 22:44:11 +0530

    LeetCode: Reverse Words in a String

--- leetcode/reverse-words-in-a-string/reverse-words-in-a-string.java
+++ leetcode/reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -11,7 +11,7 @@ class Solution {
             }
             right = left;
 
-            while(left != ' '){
+            while(left left != ' '){
                 left--;
             }
             result.append(left)
