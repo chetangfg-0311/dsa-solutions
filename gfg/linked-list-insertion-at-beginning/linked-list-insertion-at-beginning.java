9d824b367bb45d7cb77526f42ef1d65191da2a91
chetangfg-0311 <chetangfg@gmail.com>
2026-08-31 23:24:20 +0530

    GeeksforGeeks: For Python3 Platform

---
+++ gfg/linked-list-insertion-at-beginning/linked-list-insertion-at-beginning.java
@@ -0,0 +1,20 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertAtFront(Node head, int x) {
+        // code here
+        Node first = new Node(x);
+        
+        first.next = head;
+        
+        return first;
+    }
+}
