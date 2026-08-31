77f6f6aa612327110d85b08bdebd4c09626da05a
chetangfg-0311 <chetangfg@gmail.com>
2026-08-31 23:25:19 +0530

    GeeksforGeeks: For Python3 Platform

---
+++ gfg/delete-head-of-linked-list/delete-head-of-linked-list.java
@@ -0,0 +1,19 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = next;
+    }
+}
+*/
+class Solution {
+    public Node deleteHead(Node head) {
+        // code here
+        head = head.next;
+        return head;
+    }
+}
