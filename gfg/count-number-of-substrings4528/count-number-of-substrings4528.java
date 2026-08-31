8b3ad2eff3d8effbf744affb085f7e79105fc922
chetangfg-0311 <chetangfg@gmail.com>
2026-08-31 20:47:46 +0530

    GeeksforGeeks: 75 DAYS GFG POTD ARTICLE CHALLENGE CHALLENGE(DAY 42/75)

--- gfg/count-number-of-substrings4528/count-number-of-substrings4528.java
+++ gfg/count-number-of-substrings4528/count-number-of-substrings4528.java
@@ -1,29 +1,36 @@
-class Solution:
-    def count_substrings_with_k_distinct_characters(self, s, k):
-        count = 0  # Initialize a counter for the substrings
-        character_count = dict()  # Initialize a dictionary to track character counts
-        start_index = 0  # Initialize the starting index of the current substring
-
-
-        for end_index in range(len(s)):
-            character_count[s[end_index]] = character_count.get(s[end_index], 0) + 1
-
-
-            while len(character_count) > k:
-                # Remove characters from the start of the substring until we have 'k' distinct characters
-                first_char = s[start_index]
-                if character_count[first_char] == 1:
-                    del character_count[first_char]
-                else:
-                    character_count[first_char] -= 1
-                start_index += 1
-
-
-            count += (end_index - start_index + 1)  # Increment the count based on substring length
-
-
-        return count
-    
-    def countSubstr (self, s, k):
-        # Code here
-        return self.count_substrings_with_k_distinct_characters(s, k) - self.count_substrings_with_k_distinct_characters(s, k - 1)
+while(idx2 < s.length()){
+            while(idx1 < s.length() && map1.size() <= k){
+                map1.put(s.charAt(idx1), map1.getOrDefault(s.charAt(idx1), 0) + 1);
+                
+                if(map1.size() > k){
+                    map1.remove(s.charAt(idx1));
+                    break;
+                }
+                idx1++;
+            }
+            
+            while(idx2 < s.length() && map2.size() < k){
+                map2.put(s.charAt(idx2), map2.getOrDefault(s.charAt(idx2), 0) + 1);
+                if(map2.size() == k){
+                    map2.remove(s.charAt(idx2));
+                    break;
+                }
+                idx2++;
+            }
+            
+            while(map1.size() == k && map2.size() == k - 1){
+                
+                totalSubstrings += idx1 - idx2; 
+                
+                map1.put(s.charAt(idx), map1.get(s.charAt(idx)) - 1);
+                map2.put(s.charAt(idx), map2.get(s.charAt(idx)) - 1);
+                
+                if(map1.get(s.charAt(idx)) == 0){
+                    map1.remove(s.charAt(idx));
+                }
+                if(map2.get(s.charAt(idx)) == 0){
+                    map2.remove(s.charAt(idx));
+                }
+                idx++;
+            }
+        }
