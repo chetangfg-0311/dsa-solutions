ba368e8b470c58f6cb324283961f55f267d3b8be
chetangfg-0311 <chetangfg@gmail.com>
2026-08-31 23:09:51 +0530

    LeetCode: Reverse Words in a String

--- leetcode/reverse-words-in-a-string/reverse-words-in-a-string.java
+++ leetcode/reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,26 +1,26 @@
-ution {
-    public String reverseWords(String s) {
-        StringBuilder sb = new StringBuilder();
-
-        int left = s.length() - 1;
-        int right = left;
-
-        while(left > -1){
-            while(left > -1 && s.charAt(left) == ' '){
-                left--;
-            }
-            right = left + 1;
-
-            while(left > -1 && s.charAt(left) != ' '){
-                left--;
-            }
-            System.out.println("Left = " + left + " Right = " + right);
-            if(left + 1 != right){
-                sb.append(s.substring(left + 1, right) + " ");
-            }
-        }
-
-        String result = sb.toString();
-        return result.substring(0, result.length() - 1);
-    }
-}
+1class Solution {
+2    public String reverseWords(String s) {
+3        StringBuilder sb = new StringBuilder();
+4
+5        int left = s.length() - 1;
+6        int right = left;
+7
+8        while(left > -1){
+9            while(left > -1 && s.charAt(left) == ' '){
+10                left--;
+11            }
+12            right = left + 1;
+13
+14            while(left > -1 && s.charAt(left) != ' '){
+15                left--;
+16            }
+17            System.out.println("Left = " + left + " Right = " + right);
+18            if(left + 1 != right){
+19                sb.append(s.substring(left + 1, right) + " ");
+20            }
+21        }
+22
+23        String result = sb.toString();
+24        return result.substring(0, result.length() - 1);
+25    }
+26}
